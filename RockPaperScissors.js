e7c95b4a4ba4cd3f100bab4f6313d5b99c2da7fc
Charbel <charbelelbateh@gmail.com>
2022-07-14 11:43:20 +0300

    Added a UI

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,4 +1,43 @@
+//Possible choices for rps
+let computerChoice = ["Rock","Paper","Scissors"]
+
+//Generates random selectors for the computer choice
+function computerPlay(){
+    let randomNum=Math.floor(Math.random()*3)
+    return computerChoice[randomNum];
+}
+
+//Variables and Query selectors
+var computerSelection = computerPlay();
+let score;
+var computerScore = 0;
+var playerScore = 0;
+const buttons = document.querySelectorAll("button");
+const p = document.querySelector("p");
+const div =document.querySelector("div.Scores")
+
+//Displays scores at the start
+div.innerHTML = `
+<p>Computer Score:${computerScore}</p>
+<p>Player Score:${playerScore}</p>
+`
+
+//Makes The buttons take the players choice
+buttons.forEach(button =>
+    button.addEventListener('click',()=>{
+        computerSelection = computerPlay();
+        p.innerText=`${playRound(button.classList.value, computerSelection)}`
+    })
+)
+
+//Function that chooses the winners and updates the scores
 function playRound(playerSelection, computerSelection) {
+    function lose(){
+        return "You lose! "+computerSelection+" beats "+playerSelection;
+    }
+    function win(){
+        return "You Win! "+playerSelection+" beats "+computerSelection;
+    }
     if(playerSelection.toLowerCase()===computerSelection.toLowerCase()){
         return "It's a tie!";
     }else if(computerSelection === "Error"){
@@ -6,51 +45,38 @@ function playRound(playerSelection, computerSelection) {
     }else{
         switch(playerSelection.toLowerCase()){
             case "rock":
-                if(computerSelection==="Paper")
-                    return "You lose! "+computerSelection+" beats "+playerSelection;
-                else if (computerSelection==="Scissors")
-                    return "You Win! "+playerSelection+" beats "+computerSelection;
+                if(computerSelection==="Paper"){
+                    computerScore++;
+                    score = lose();
+                }else if (computerSelection==="Scissors"){
+                    playerScore++;
+                    score = win();
+                }
+                break;
             case "paper":
-                if(computerSelection==="Scissors")
-                    return "You Win! "+playerSelection+" beats "+computerSelection;
-                else if(computerSelection==="Rock")
-                    return "You lose! "+computerSelection+" beats "+playerSelection;
+                if(computerSelection==="Rock"){
+                    playerScore++;
+                    score = win();
+                }else if(computerSelection==="Scissors"){
+                    computerScore++;
+                    score = lose();
+                }
+                break;
             case "scissors":
-                if(computerSelection==="Paper")
-                    return "You Win! "+playerSelection+" beats "+computerSelection;
-                else if(computerSelection==="Rock")
-                    return "You lose! "+computerSelection+" beats "+playerSelection;
+                if(computerSelection==="Paper"){
+                    playerScore++;
+                    score = win();
+                }else if(computerSelection==="Rock"){
+                    computerScore++;
+                    score = lose();
+                }
+                break;
         }
+            div.innerHTML = `
+                <p>Computer Score:${computerScore}</p>
+                <p>Player Score:${playerScore}</p>
+                `
+
+        return score;
     }
 }
-function computerPlay(){
-    let randomNum=Math.floor(Math.random()*3+1)
-    switch (randomNum){
-        case 1:
-            return "Rock";
-            break;
-        case 2:
-            return "Paper";
-            break;
-        case 3:
-            return "Scissors";
-            break;
-        default:
-            return "Error";
-    }
-}
-function game(){
-    for(let i=1; i<5; i++){
-        let playerSelection = prompt("Please input your choice:");
-        let computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
-    let playerSelection = prompt("Ready to play?");
-    if (playerSelection.toLowerCase()==="yes"){
-        let computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-        game();
-    }else if(playerSelection.toLowerCase()==="no"){
-        alert("okay!");
-    }
